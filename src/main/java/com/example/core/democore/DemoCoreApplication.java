d65f6b534792e078afa731e1e85d2648224e9f6a
ivanyu <yobuke@126.com>
2017-08-31 15:26:43 +0800

    初始结构

---
+++ src/main/java/com/example/core/democore/DemoCoreApplication.java
@@ -0,0 +1,12 @@
+package com.example.core.democore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCoreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/core/democore/DemoCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.core.democore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
